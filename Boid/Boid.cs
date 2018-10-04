08a939d4ed5fe228dce7e293becd6caa2f1ffe57
Amarnath Parthiban <amarnath.parthiban@mail.utoronto.ca>
2018-10-04 17:33:37 -0400

    uploaded all files

---
+++ Boid/Boid.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace Engine
+{
+    public class Boid
+    {
+        Vector curr_pos;
+        Vector vel;
+
+        public double x
+        {
+            get { return curr_pos.X; }
+        }
+
+        public double y
+        {
+            get { return curr_pos.Y; }
+        }
+
+        const int radius_flock = 50;
+        const int radius_space = 5;
+        const int limit_vel = 5;
+
+        const int x_min = 50;
+        const int x_max = 650;
+        const int y_min = 50;
+        const int y_max = 550;
+
+        public static List<Boid> all_boids = new List<Boid>();
+
+        public Boid(double x, double y, double dx, double dy)
+        {
+            this.curr_pos = new Vector(x, y);
+            this.vel = new Vector(dx, dy);
+            all_boids.Add(this);
+        }
+
+        public void Move()
+        {
+            Vector v1 = Rule1();
+            Vector v2 = Rule2();
+            Vector v3 = Rule3();
+
+            vel += v1 + v2 + v3;
+            vel += Bounds();
+            LimitVeloicity();
+
+            curr_pos += vel;
+        }
+
+        private Vector Rule1()
+        {
+            Vector v1 = new Vector(0, 0);
+            int n = 0;
+
+            foreach(Boid b in all_boids)
+            {
+                if (!this.Equals(b) && Distance(this, b) < radius_flock)
+                {
+                    v1 += b.curr_pos;
+                    n++;
+                }
+            }
+
+            if (n == 0)
+                n++;
+
+            v1 /= n;
+
+            return (v1 - curr_pos) / 100;
+        }
+
+        private Vector Rule2()
+        {
+            Vector v2 = new Vector(vel.X, vel.Y);
+
+            foreach (Boid b in all_boids)
+            {
+                if (!this.Equals(b) && Distance(this, b) < radius_space)
+                {
+                    v2 -= b.curr_pos - curr_pos;
+                }
+            }
+
+            return v2;
+        }
+
+        private Vector Rule3()
+        {
+            Vector v3 = new Vector(vel.X, vel.Y);
+            int n = 0;
+
+            foreach (Boid b in all_boids)
+            {
+                if (!this.Equals(b) && Distance(this, b) < radius_flock)
+                {
+                    v3 += b.vel;
+                    n++;
+                }
+            }
+
+            if (n == 0)
+                n++;
+
+            v3 /= n;
+
+            return (v3 - vel) / 8;
+        }
+
+        private void LimitVeloicity()
+        {
+            if (vel.Length > limit_vel)
+            {
+                vel = vel / vel.Length * limit_vel;
+            }
+        }
+
+        private Vector Bounds()
+        {
+            Vector b = new Vector(0, 0);
+
+            if (curr_pos.X < x_min)
+                b.X = 3;
+            else if (curr_pos.X > x_max)
+                b.X = -3;
+
+            if (curr_pos.Y < y_min)
+                b.Y = 2;
+            else if (curr_pos.Y > y_max)
+                b.Y = -2;
+
+            return b;
+        }
+
+        private static double Distance(Boid a, Boid b)
+        {
+            return (double) (b.curr_pos - a.curr_pos).Length ;
+        }
+
+    }
+ 
+}
---
+++ Flocks/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Engine;
+using System.Diagnostics;
+
+namespace Flocks
+{
+    public partial class Flock : Form
+    {
+
+        List<Boid> all_boids;
+        Timer tm;
+
+        public Flock()
+        {
+            InitializeComponent();
+
+            all_boids = new List<Boid>();
+            tm = new Timer();
+            tm.Tick += new EventHandler(Update);
+            tm.Interval = 10;
+            tm.Enabled = true;
+            tm.Start();
+
+        }
+
+        private void Add_Boid(object sender, MouseEventArgs e)
+        {
+            Random r = new Random();
+            Point m = this.PointToClient(Cursor.Position);
+            for (int _ = 0; _ < 10; _++)
+                all_boids.Add(new Boid(m.X, m.Y, r.Next(-5, 5), r.Next(-3, 3)));
+        }
+
+        private void Boid_Draw(object sender, PaintEventArgs e)
+        {
+            foreach (Boid b in all_boids) {
+                e.Graphics.DrawEllipse(new Pen(Color.Black, 1), (float) b.x, (float) b.y, 5, 5);
+            }
+        }
+
+        private void Update(object sender, EventArgs e)
+        {
+            Refresh();
+            
+            foreach (Boid b in all_boids)
+            {
+                b.Move();
+            }
+        }
+
+        private void Flock_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
